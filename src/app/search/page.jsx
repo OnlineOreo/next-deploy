abbd6efb5444f166230c1cf92cce946928aaf72e
OnlineOreo <mannurawat4190@gmail.com>
2025-05-06 23:14:27 +0530

    build error solve

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const SearchBar = () => {
@@ -22,11 +22,11 @@ const SearchBar = () => {
     }
   }, [searchParams]);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (filterType === "resource_types_string") {
-      setSearchText("book"); 
+      setSearchText("book");
     }
-  },[filterType])
+  }, [filterType])
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -55,42 +55,44 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <form id="search_form" className="flex border" onSubmit={handleSubmit}>
-        <select value={filterType} onChange={(e) => setFilterType(e.target.value)}>
-          <option value="datacite_titles">Title</option>
-          <option value="datacite_creators">Author</option>
-          <option value="dc_publishers">Publisher</option>
-          <option value="resource_types_string">Resource Types</option>
-          <option value="college_category">Subject</option>
-        </select>
-
-        {filterType === "resource_types_string" ? (
-          <select value={searchText} onChange={(e) => setSearchText(e.target.value)} style={{ width: "60%" }}>
-            <option value="book">Print Collection</option>
-            <option value="e-book">e-book</option>
-            <option value="e-journals">e-journals</option>
-            <option value="Video">Video</option>
-            <option value="audio">Audio</option>
+    <Suspense>
+      <div className="w-screen h-screen flex items-center justify-center">
+        <form id="search_form" className="flex border" onSubmit={handleSubmit}>
+          <select value={filterType} onChange={(e) => setFilterType(e.target.value)}>
+            <option value="datacite_titles">Title</option>
+            <option value="datacite_creators">Author</option>
+            <option value="dc_publishers">Publisher</option>
+            <option value="resource_types_string">Resource Types</option>
+            <option value="college_category">Subject</option>
           </select>
-        ) : (
-          <input
-            type="text"
-            value={searchText}
-            placeholder="Search with/without any keyword"
-            onChange={(e) => setSearchText(e.target.value)}
-            style={{ width: "60%" }}
-          />
-        )}
 
-        <button type="submit">
-          <img
-            alt="Search"
-            src="https://wp.alithemes.com/html/evara/evara-frontend/assets/imgs/theme/icons/search.png"
-          />
-        </button>
-      </form>
-    </div>
+          {filterType === "resource_types_string" ? (
+            <select value={searchText} onChange={(e) => setSearchText(e.target.value)} style={{ width: "60%" }}>
+              <option value="book">Print Collection</option>
+              <option value="e-book">e-book</option>
+              <option value="e-journals">e-journals</option>
+              <option value="Video">Video</option>
+              <option value="audio">Audio</option>
+            </select>
+          ) : (
+            <input
+              type="text"
+              value={searchText}
+              placeholder="Search with/without any keyword"
+              onChange={(e) => setSearchText(e.target.value)}
+              style={{ width: "60%" }}
+            />
+          )}
+
+          <button type="submit">
+            <img
+              alt="Search"
+              src="https://wp.alithemes.com/html/evara/evara-frontend/assets/imgs/theme/icons/search.png"
+            />
+          </button>
+        </form>
+      </div>
+    </Suspense>
   );
 };
 
